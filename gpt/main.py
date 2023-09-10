4212ec7d3ab9cdd98d7ec7749d2e4f642d0a021a
junghun Lee <xy7648@gmail.com>
2023-09-10 23:57:22 +0900

    add prompt

--- gpt/main.py
+++ gpt/main.py
@@ -1,14 +1,43 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import openai
+from langchain.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
+import os
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain import PromptTemplate
+from dotenv import load_dotenv
 
-openai.api_key = "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"
+# uvicorn main:app --reload
+load_dotenv(verbose=True)
+
+# loader = PyPDFLoader('pdf\Principles of Billet Soft-reduction and Consequences for Continuous Casting.pdf') # load file
+loader = PyPDFDirectoryLoader('pdf/') # load file
+pages = loader.load_and_split() # split document
+db = Chroma.from_documents(pages, OpenAIEmbeddings(), persist_directory="./vectorstore") # embedding -> vertor store
 
 class QuestionRequest(BaseModel):
     question: str # question의 값은 str이어야 함
 
 app = FastAPI()
 
+prompt_template = PromptTemplate(
+    input_variables=["context", "query"],
+    template="""You are a steel engineer who answers questions about PDFs when they come in.
+                [query] Find the question within [context] and explain it kindly and accurately.
+                Please explain so that quantitative figures are included rather than qualitative expressions.
+                If there's anything you don't know, "I don't know. Ask me another question."
+                Please answer in Korean
+                =====================================================================
+                [context]
+                {context}
+                
+                =====================================================================
+                [query]
+                {query}
+                """
+)
 
 @app.get("/")
 async def root():
@@ -17,10 +46,16 @@ async def root():
 @app.post("/chat")
 async def chat(request_body: QuestionRequest): # fastapi 규칙
     # print(request_body.question)
+    query = request_body.question
+    
+    searched = db.similarity_search(query, k=3) # k:3 질문과 유사한거 3개 찾아라
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo", # gpt-4-0613 # gpt-3.5-turbo
         messages=[
-            {"role": "user", "content": request_body.question}, # 질문을 openai에게 준다.
+            {
+                "role": "user",
+                "content": prompt_template.format(context=searched, query=query)
+            }, # 질문을 openai에게 준다. request_body.question가 query임
         ]
     )
     # print(answer)
